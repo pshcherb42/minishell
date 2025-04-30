4d67eff2c11b8b8d367f1533a0531ea986bca1c0
Anna Kreise <akreise@car15s5.42barcelona.com>
2025-04-30 18:53:17 +0200

    ana3

--- executor/exec.c
+++ executor/exec.c
@@ -3,46 +3,62 @@
 /*                                                        :::      ::::::::   */
 /*   exec.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pshcherb <pshcherb@student.42.fr>          +#+  +:+       +#+        */
+/*   By: akreise <akreise@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 17:14:46 by pshcherb          #+#    #+#             */
-/*   Updated: 2025/04/15 12:15:44 by pshcherb         ###   ########.fr       */
+/*   Updated: 2025/04/27 19:45:16 by akreise          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-int	open_redirs(t_cmd *cmd)
+static int	open_infile(t_cmd *cmd)//файл для ввода?-cat < input.txt — здесь input.txt это infile
+{
+	int	fd;//файловый дескриптор (номер открытого файла)
+
+	if (!cmd->infile)//если файл пустой не сущ
+		return (1);
+	if (cmd->heredoc)//обрабатывается отдельно
+		return (1);
+	fd = open(cmd->infile, O_RDONLY);//открыли только для чтения
+	if (fd < 0)//файл открыть не получилось (например, его нет)
+	{
+		perror(cmd->infile);
+		return (0);
+	}
+	dup2(fd, STDIN_FILENO);//Подменяем стандартный ввод (stdin-клавиатура) на наш файл
+	close(fd);//закрываем старый файловый дескриптор (fd)
+	return (1);
+}
+
+static int	open_outfile(t_cmd *cmd)
 {
 	int	fd;
+	
+	if (!cmd->outfile)
+		return (1);
+	if (cmd->append)//если добавляем в конец <<
+		fd = open(cmd->outfile, O_WRONLY | O_CREAT | O_APPEND, 0644);//апп-добавить вконец, креат создаст файл - 0644 — права на файл: владелец может читать/писать, остальные только читать
+	else//если перезаписываем файл >
+		fd = open(cmd->outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);//O_TRUNC — очищаем файл перед записью.
+	if (fd < 0)
+	{
+		perror(cmd->outfile);
+		return (0);
+	}
+	dup2(fd, STDOUT_FILENO);//Подменяем стандартный ввод (stdout-экран) на наш файл
+	close(fd);//заменили 0-ввод клавы на номер файла, поэтому этот очистили(остается 0 read(0, buffer, 100))
+	return (1);
+}
 
-	if (cmd->infile)
-	{
-		if (cmd->heredoc)
-			return (1);
-		fd = open(cmd->infile, O_RDONLY);
-		if (fd < 0)
-		{
-			perror(cmd->infile);
-			return (0);
-		}
-		dup2(fd, STDIN_FILENO);
-		close(fd);
-	}
-	if (cmd->outfile)
-	{
-		if (cmd->append)
-			fd = open(cmd->outfile, O_WRONLY | O_CREAT | O_APPEND, 0644);
-		else
-			fd = open(cmd->outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-		if (fd < 0)
-		{
-			perror(cmd->outfile);
-			return (0);
-		}
-		dup2(fd, STDOUT_FILENO);
-		close(fd);
-	}
+int	open_redirs(t_cmd *cmd)//открывает файлы, которые указаны у команды для редиректа ввода/вывода
+{
+	int	fd;//файловый дескриптор (номер открытого файла)
+
+	if (!open_infile(cmd))//если не удалось открыть файл и пришла ошибка
+		return (0);
+	if (!open_outfile(cmd))
+		return (0);
 	return (1);
 }
 
