7bc7a48e8356df7cdcabda211ae7c04bdce9c955
Anna Kreise <akreise@car15s5.42barcelona.com>
2025-04-30 20:06:25 +0200

    anna exec1

--- executor/exec.c
+++ executor/exec.c
@@ -6,7 +6,7 @@
 /*   By: akreise <akreise@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 17:14:46 by pshcherb          #+#    #+#             */
-/*   Updated: 2025/04/30 19:04:32 by akreise          ###   ########.fr       */
+/*   Updated: 2025/04/30 20:05:03 by akreise          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,7 +60,7 @@ int	open_redirs(t_cmd *cmd)//открывает файлы, которые ук�
 	return (1);
 }
 
-int	execute_cmds(t_cmd *cmd, char ***envp)
+/*int	execute_cmds(t_cmd *cmd, char ***envp)
 {
 	int		pipefd[2];
 	int		prev_fd;
@@ -145,6 +145,100 @@ int	execute_cmds(t_cmd *cmd, char ***envp)
 		}
 		signal(SIGINT, handle_sigint);
 		signal(SIGQUIT, SIG_IGN);
+		cmd = cmd->next;
+	}
+	return (WEXITSTATUS(status));
+}*/
+
+static void	setup_child_signals(void)
+{
+	signal(SIGINT, SIG_DFL);
+	signal(SIGQUIT, SIG_DFL);
+}
+
+static int	setup_child_io(t_cmd *cmd, int prev_fd, int *pipefd)
+{
+	if (prev_fd != -1)
+	{
+		dup2(prev_fd, STDIN_FILENO);
+		close(prev_fd);
+	}
+	if (cmd->next)
+	{
+		close(pipefd[0]);
+		dup2(pipefd[1], STDOUT_FILENO);
+		close(pipefd[1]);
+	}
+	if (!open_redirs(cmd))
+		exit(EXIT_FAILURE);
+	return (1);
+}
+
+static void	execute_child_command(t_cmd *cmd, char ***envp)
+{
+	char	*path;
+
+	if (is_builtin(cmd->args[0]))
+		exit(exec_builtin(cmd, envp));
+	path = get_cmd_path(cmd->args[0], *envp);
+	if (!path)
+	{
+		ft_pstr(2, "minishell: %s: command not found\n", cmd->args[0]);
+		exit(127);
+	}
+	execve(path, cmd->args, *envp);
+	perror("execve");
+	exit(EXIT_FAILURE);
+}
+
+static void	run_child_process(t_cmd *cmd, int prev_fd, int *pipefd, char ***envp)
+{
+	setup_child_signals();
+	setup_child_io(cmd, prev_fd, pipefd);
+	execute_child_command(cmd, envp);
+}
+
+static void	handle_parent_pipe(t_cmd *cmd, int *pipefd, int *prev_fd)
+{
+	if (*prev_fd != -1)
+		close(*prev_fd);
+	if (cmd->next)
+	{
+		close(pipefd[1]);
+		*prev_fd = pipefd[0];
+	}
+}
+
+static void	handle_child_exit_status(int status)
+{
+	if (WIFSIGNALED(status) && WTERMSIG(status) == SIGQUIT)
+		write(2, "Quit (core dumped)\n", 20);
+}
+
+int	execute_cmds(t_cmd *cmd, char ***envp)
+{
+	int		pipefd[2];
+	int		prev_fd;
+	pid_t	pid;
+	int		status;
+
+	prev_fd = -1;
+	if (!cmd || !cmd->args || !cmd->args[0])
+		return (1);
+	while (cmd)
+	{
+		if (is_builtin(cmd->args[0]) && is_parent_builtin(cmd->args[0]))
+			return (exec_builtin(cmd, envp));
+		if (cmd->next && pipe(pipefd) < 0)
+			return (perror("pipe"), 1);
+		pid = fork();
+		if (pid < 0)
+			return (perror("fork"), 1);
+		if (pid == 0)
+			run_child_process(cmd, prev_fd, pipefd, envp);
+		handle_parent_pipe(cmd, pipefd, &prev_fd);
+		waitpid(pid, &status, 0);
+		handle_child_exit_status(status);
 		cmd = cmd->next;
 	}
 	return (WEXITSTATUS(status));
