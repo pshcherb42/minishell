0170bdd92b8dde56919be82ec735bb8a9a0bffe4
Polina Shcherbina <pshcherb@car8s5.42barcelona.com>
2025-05-20 15:34:53 +0200

    heredoc funcionando

--- redirection/heredoc.c
+++ redirection/heredoc.c
@@ -6,33 +6,17 @@
 /*   By: pshcherb <pshcherb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 16:42:32 by pshcherb          #+#    #+#             */
-/*   Updated: 2025/05/16 20:17:22 by pshcherb         ###   ########.fr       */
+/*   Updated: 2025/05/20 15:18:45 by pshcherb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-static int	is_quoted(const char *str)
-{
-	size_t len = ft_strlen(str);
-	return (len >= 2 && (str[0] == '\'' || str[0] == '"')
-			&& str[len - 1] == str[0]);
-}
 
-static char	*strip_quotes(const char *str)
-{
-	size_t len = ft_strlen(str);
-	if (is_quoted(str))
-		return (ft_substr(str, 1, len - 2));
-	return (ft_strdup(str));
-}
 
-void	handle_here_doc(int *p_fd, const char *delimiter, int expand)
+void	handle_here_doc(int *p_fd, const char *delimiter)
 {
-	char			*line;
-	t_split_vars	vars;
-	char			*expanded;
-
+	char	*line;
 	while (1)
 	{
 		line = readline("> ");
@@ -43,18 +27,8 @@ void	handle_here_doc(int *p_fd, const char *delimiter, int expand)
 			free(line);
 			break ;
 		}
-		if (expand)
-		{
-			expanded = expand_token(vars.expanded);
-			write(p_fd[1], expanded, ft_strlen(expanded));
-			write(p_fd[1], "\n", 1);
-			free(expanded);
-		}
-		else
-		{
-			write(p_fd[1], line, ft_strlen(line));
-			write(p_fd[1], "\n", 1);
-		}
+		write(p_fd[1], line, ft_strlen(line));
+		write(p_fd[1], "\n", 1);
 		free(line);
 	}
 }
@@ -62,20 +36,13 @@ void	handle_here_doc(int *p_fd, const char *delimiter, int expand)
 int	handle_heredoc(const char *delimiter)
 {
 	int		p_fd[2];
-	char	*clean_delim;
-	int		expand;
 
 	if (pipe(p_fd) == -1)
 	{
 		perror("pipe");
 		return (-1);
 	}
-	expand = !is_quoted(delimiter);
-	clean_delim = strip_quotes(delimiter);
-	if (!clean_delim)
-		return (-1);
-	handle_here_doc(p_fd, delimiter, expand);
-	free(clean_delim);
+	handle_here_doc(p_fd, delimiter);
 	close(p_fd[1]);
 	return (p_fd[0]);
 }
