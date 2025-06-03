0918d736fd2fbf3a55f69e8c0dcace6c216a88a8
Polina Shcherbina <pshcherb@car13s6.42barcelona.com>
2025-06-03 15:17:20 +0200

    all works

--- parser/redirection.c
+++ parser/redirection.c
@@ -6,7 +6,7 @@
 /*   By: pshcherb <pshcherb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 17:03:08 by pshcherb          #+#    #+#             */
-/*   Updated: 2025/06/03 12:48:37 by pshcherb         ###   ########.fr       */
+/*   Updated: 2025/06/03 14:49:31 by pshcherb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,22 +61,28 @@ static int	handle_input_redirect(t_cmd *cmd, char **tokens, int i)
 
 static int	handle_heredoc_redirect(t_cmd *cmd, char **tokens, int i)
 {
+	char	*temp_infile;
+	
 	if (!tokens[i + 1])
 	{
 		ft_printf("minishell: syntax error near unexpected token `newline'\n");
 		return (-1);
 	}
-	if (cmd->infile)
-		free(cmd->infile);
-	if (cmd->heredoc_file)
-		cleanup_temp_file(cmd->heredoc_file);
-	cmd->infile = ft_strdup(tokens[++i]);
-	cmd->heredoc = 1;
-	if (handle_here_fork(cmd->infile, &cmd->heredoc_file) == -1)
+	temp_infile = ft_strdup(tokens[++i]);
+	if (!temp_infile)
+		return (-1);
+	if (handle_here_fork(temp_infile, &cmd->heredoc_file) == -1)
 	{
+		free(temp_infile);
 		cmd->heredoc_interrupted = 1;
 		return (-2);
 	}
+	if (cmd->infile)
+		free(cmd->infile);
+	if (cmd->heredoc_file && cmd->heredoc)
+		cleanup_temp_file(cmd->heredoc_file);
+	cmd->infile = temp_infile;
+	cmd->heredoc = 1;
 	return (i + 1);
 }
 
